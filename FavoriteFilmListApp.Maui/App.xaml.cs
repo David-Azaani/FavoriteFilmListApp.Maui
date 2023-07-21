fb8557ba5d2164618be032512f9eff9aec6f01db
f00000b <f00000b@gmail.com>
2023-07-21 23:10:07 +0330

    Add project files.

---
+++ FavoriteFilmListApp.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FavoriteFilmListApp.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FavoriteFilmListApp.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FavoriteFilmListApp.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
